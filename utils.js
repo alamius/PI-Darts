d64537bf51f83e612fb1dcf25abf338333e7027a
alamius <anton.obersteiner@gmail.com>
2017-07-28 21:20:33 +0200

    Update utils.js
    getting more space in the output field

--- utils.js
+++ utils.js
@@ -18,7 +18,7 @@ Array.prototype.get_average = function(){
 function info() { //div-id, p-element-id, text consisting of a desciprion and a number
     add_text_node("div0", "total",  "total dots: "              + str(dots_total));
     add_text_node("div0", "circl",  "dots in the circle: "      + str(dots_circl));
-    add_text_node("div0", "sqare",  "dots outside the circle: " + str(dots_total - dots_circl));
+    //add_text_node("div0", "sqare",  "dots outside the circle: " + str(dots_total - dots_circl));
     add_text_node("div0", "pi",     "circle / total * 4: "      + str(pi_approx));
     add_text_node("div0", "graph",  "graph (press [G]): "       + (graph_show ? 'drawing' : 'not drawing'));
     add_text_node("div0", "avg",    "graph: averages (press [A]): " + (graph_averages ? 'true' : 'false'));
